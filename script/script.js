2035232fb4c2d6885466e72a81f2a4ed7287a1a3
Rafael da Costa Silva <rafael1501.oficial@gmail.com>
2024-05-16 13:50:19 -0300

    concluída

--- script/script.js
+++ script/script.js
@@ -1,117 +1,123 @@
-const tbody = document.querySelector("tbody");
-const addTask = document.getElementById("addTaskBtn");
+const openFormBtn = document.getElementById("openFormBtn"); // Botão para abrir o formulário
+const closeFormBtn = document.getElementById("closeFormBtn"); //Botão para fechar o formulário
+const form = document.getElementById("form"); // Formulário
 
+const title = document.getElementById("input"); // Input que recebe título da tarefa
+const term = document.getElementById("term"); // Input que recebe a data de entrega da tarefa
+
+const addTask = document.getElementById("addTaskBtn"); // Botão para adicionar a terefa na tabela
+
+const confirmDelete = document.getElementById("confirmDelete"); // Confirmação para apagar tarefa
+const cancelDelete = document.getElementById("cancelDelete");
+
+cancelDelete.addEventListener("click", () => {
+    confirmDelete.style.display = "none"
+})
+
+// Adiciona a função de abrir no botão de abrir o formulário
+openFormBtn.addEventListener("click", () => {
+    form.style.display = "flex";
+});
+
+// Função que fecha o formulário
+const closeForm = () => {
+    form.style.display = "none";
+};
+
+//Botão que fecha o formulário
+closeFormBtn.addEventListener("click", () => {
+    closeForm();
+});
+
+// Array que recebe as tarefas
 let saveTasks = [];
 
-let info = {
-    title: "",
-    data: "",
-    status: "",
-};
-
+// Captura o título colocado no input dentro do formulário
 const createTitle = () => {
-    const titleInput = document.getElementById("titleInput");
-    info.title = titleInput.value;
+    return title.value;
 };
 
+// Captura e formata a data colocado no input dentro do formulário
 const createData = () => {
-    const dataAtual = new Date();
-    const dia = dataAtual.getDate();
-    const mes = dataAtual.getMonth();
-    const ano = dataAtual.getFullYear();
-    const horas = dataAtual.getHours();
-    const minutos = dataAtual.getMinutes();
+    const dateValue = term.value;
 
-    const meses = [
-        "Janeiro",
-        "Fevereiro",
-        "Março",
-        "Abril",
-        "Maio",
-        "Junho",
-        "Julho",
-        "Agosto",
-        "Setembro",
-        "Outubro",
-        "Novembro",
-        "Dezembro",
-    ];
+    const selectedDate = new Date(dateValue);
+    const day = String(selectedDate.getDate()).padStart(2, "0");
+    const month = String(selectedDate.getMonth() + 1).padStart(2, "0");
+    const year = selectedDate.getFullYear();
 
-    const mesFormatado = meses[mes];
-
-    const dataFormatada = `${dia} de ${mesFormatado} de ${ano}`;
-
-    const horarioFormatado = `${horas}:${minutos}`;
-
-    const dataAtualFormatada = `${dataFormatada} ${horarioFormatado}`;
-
-    info.data = dataAtualFormatada;
-};
-
-const createElements = () => {
-    const tr = document.createElement("tr");
-    const tdTitle = document.createElement("td");
-    const tdData = document.createElement("td");
-    const tdSelect = document.createElement("td");
-    const select = document.createElement("select");
-    const tdButtons = document.createElement("td");
-    const deleteButton = document.createElement("button");
-    const span = document.createElement("span");
-
-    tr.appendChild(tdTitle);
-    tdTitle.innerHTML = info.title;
-
-    tr.appendChild(tdData);
-    tdData.innerHTML = info.data;
-
-    tr.appendChild(tdSelect);
-    tdSelect.appendChild(select);
-    const options = `
-        <option value="pendente">pendente</option>
-        <option value="andamento">em andamento</option>
-        <option value="concluida">concluída</option>
-        `;
-
-    select.innerHTML = options;
-
-    info.status = "pendente";
-
-    select.addEventListener("change", (event) => {
-        info.status = event.target.value;
-        saveTasks.push(info);
-    });
-
-    tr.appendChild(tdButtons);
-    tdButtons.appendChild(deleteButton);
-    deleteButton.classList.add('action-btn');
-
-    span.classList.add("material-symbols-outlined");
-    span.textContent = "delete";
-
-    deleteButton.appendChild(span);
-
-    deleteButton.addEventListener("click", function() {
-        tbody.removeChild(tr);
-    });
-
-    tbody.appendChild(tr);
+    return `${day}/${month}/${year}`;
 };
 
 addTask.addEventListener("click", () => {
-    createTitle();
-    createData();
+    const info = {
+        title: createTitle(),
+        data: createData(),
+    };
 
-    createElements();
-
+    let saveTasks = JSON.parse(localStorage.getItem("task")) || [];
     saveTasks.push(info);
 
-    saveTasksLS();
+    const taskJSON = JSON.stringify(saveTasks);
+    localStorage.setItem("task", taskJSON);
 
-    const titleInput = document.getElementById("titleInput").value = ""
+    document.getElementById("title").value = "";
+
+    createElement(); // Chamar a função para atualizar a tabela após adicionar a nova tarefa
+
+    closeForm();
 });
 
-const saveTasksLS = () => {
-    const saveTasksJSON = JSON.stringify(saveTasks);
+const createElement = () => {
+    const taskJSON = localStorage.getItem("task");
+    const saveTasks = taskJSON ? JSON.parse(taskJSON) : [];
+    const tbody = document.querySelector("tbody");
 
-    localStorage.setItem("saveTasks", saveTasksJSON);
+    tbody.innerHTML = ""; // Limpar o conteúdo existente do tbody
+
+    saveTasks.forEach((info) => {
+        const tr = document.createElement("tr");
+        const tdTitle = document.createElement("td");
+        tdTitle.textContent = info.title;
+
+        const tdData = document.createElement("td");
+        tdData.textContent = info.data;
+
+        const tdButtons = document.createElement("td");
+        const deleteButton = document.createElement("button");
+        deleteButton.classList.add("action-btn");
+
+        const span = document.createElement("span");
+        span.classList.add("material-symbols-outlined");
+        span.textContent = "delete";
+
+        deleteButton.appendChild(span);
+        deleteButton.addEventListener("click", () => {
+            confirmDelete.style.display = "flex";
+
+            document
+                .getElementById("deleteTask")
+                .addEventListener("click", () => {
+                    const index = saveTasks.indexOf(info);
+                    if (index > -1) {
+                        saveTasks.splice(index, 1);
+                        localStorage.setItem("task", JSON.stringify(saveTasks));
+                        createElement(); // Atualizar a tabela
+
+                        confirmDelete.style.display = "none";
+                    }
+                });
+        });
+
+        tdButtons.appendChild(deleteButton);
+        tr.appendChild(tdTitle);
+        tr.appendChild(tdData);
+        tr.appendChild(tdButtons);
+        tbody.appendChild(tr); // Adicionar a nova linha à tabela
+    });
 };
+
+// Chamar createElement ao carregar a página para exibir as tarefas existentes
+window.addEventListener("load", () => {
+    createElement(); // Chamar a função ao carregar a página
+});
